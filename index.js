fce0d8186a1e58024bce70582fd17d2940a600bd
Radhe Chaudhary <136416098+radhechaudhary@users.noreply.github.com>
2024-11-08 21:02:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,122 +1,93 @@
-import express from "express";
-import pg from "pg"
-import bodyParser from "body-parser";
-import cors from "cors"
-import { configDotenv } from "dotenv";
-import { createTransport } from "nodemailer";
+import React, { useState} from 'react'
+import { Link } from 'react-router-dom'
+import Backdrop from '@mui/material/Backdrop';
+import CircularProgress from '@mui/material/CircularProgress';
+import icon from './regester icon.png'
+import TextField from '@mui/material/TextField';
 
-const PORT=4000;
-const app=express();
+function SignUp(prop) {
+    let Mystyle={display: "flex", flexDirection: "column", gap:"40px"}
+    let MyStyle2={width:"fit-content"}
 
-const transporter=createTransport({
-  service:'gmail',
-  auth: {
-    user: process.env.MY_GMAIL, // Your email address
-    pass: process.env.MY_GMAIL_PASSWORD, // Your email password or app password
-  },
-})
-
-const db=new pg.Client({
-    user: process.env.DATABASE_USER,
-    host: process.env.DATABASE_HOST,
-    database: process.env.DATABASE_DATABASE,
-    password: process.env.DATABASE_PASSSWORD,
-    port: process.env.DATABASE_PORT,
-  });
-
-  db.connect();
-  app.use(bodyParser.urlencoded({ extended: true }));
-  app.use(bodyParser.json())
-  app.use(cors())
-app.post("/login",async (req,res)=>{
-    let username=req.body.username;
-    let pass=req.body.password;
-    try
-    {
-      const result=await db.query("select password, name from users where username =$1",[username]);
-      const data=result.rows[0];
-      if(data.password===pass)
-      {
-        res.json({status:"valid", name:data.name})
+    const [values, setValues]=useState({name:"", mobile:"", username:"", password:"", buisness:"", rooms:"", mail:""});  // hook to store input values
+    function handleChange(e){       // function that triggers when any input is triggered
+      const name=e.target.name   
+      var value=e.target.value
+      if(name!=="name"){           // dont allow spaces except name
+        value=value.trim();
       }
+      setValues({
+        ...values,
+        [name]:value
+      })
+    }
+
+    function submit(e){        //submit function when form is submitted
+      e.preventDefault();
+      const name=values.name.trim()
+      setValues({
+        ...values,
+        name:name
+      })
+      if(values.username==="" || values.password===""  || values.name==="" || values.mobile==="" || values.buisness==="" || values.rooms==="" || values.mail==="" ){
+          alert("A Field cannot be blank")
+          setNextpage(1)
+        }
       else{
-        res.json("wrong password");
+          prop.signUp(values);
       }
-    }
-    
-    catch(err)
-    {
-      res.json("username not found")
-    }
-})
-
-
-app.post('/signup', async (req, res)=>{
-  const name=req.body.name;
-  const mobile=req.body.mobile;
-  const username=req.body.username;
-  const password=req.body.password;
-  const rooms=req.body.rooms;
-  const mail=req.body.mail;
-  const buisness=req.body.buisness
-
-  try{
-    const result=await db.query("insert into users (name, mobile, username,  password, buisness, mail, rooms) values($1,$2,$3,$4,$5,$6,$7) ",[name, mobile, username, password, buisness, mail, rooms]);
-    res.send("submitted")
-    const mailOptions = {
-      from: 'radhechaudhary6398@gmail.comm', // Sender address
-      to: mail, // Recipient address
-      subject: 'Automated Email from user-regester', // Subject line
-      text: 'Hello!! Thankyou for connecting with us!! Hope the journey will be great!!', // Plain text body
-      html: '<p>Hello!!<b>Thankyou for connecting with us</b>Hope the journey will be great!!</p>', // HTML body (optional)
-    };
-    transporter.sendMail(mailOptions, (error, info) => {
-      if (error) {
-        return console.log('Error occurred:', error);
-      }
-      console.log('Email sent:', info.response);
-    });
   }
-  catch(err){
-    res.send(err.message)
+  const [nextPage, setNextpage]=useState(1)
+  const [loading, setLoading]=useState(false)
+  function next(e){
+    e.preventDefault()
+    setLoading(true)
+        setLoading(false)
+        setNextpage(nextPage+1) 
   }
-})
+  function prev(e){
+    e.preventDefault()
+    setLoading(true)
+    setLoading(false)
+    setNextpage(nextPage-1)
+  }
 
-app.post('/data-submit',async(req,res)=>{
-  if(req.query.apiKey==="njknj4j43npa@mweoc43"){
-    try{
-      const result=await db.query("insert into costumers (username, name, mobile, room_no, entry_date, entry_time,  out_date, out_time, members) values($1,$2,$3,$4,$5,$6,$7,$8,$9)",
-        [req.body.username, req.body.name, req.body.mobile, req.body.roomNo, req.body.inDate, req.body.inTime, req.body.OutDate, req.body.OutTime, req.body.members]
-      )
-      res.send(req.body.name)
-      
-    }
-    catch(err){
-      res.send(err.message)
-    }
-  }
-  else{
-    res.send("not valid")
-  }
-  
-
-})
-
-app.get("/data", async(req,res)=>{
-  if(req.query.apiKey===process.env.API_KEY){
-    try{
-      const result=await db.query("Select * from costumers where username=$1",[req.query.username])
-      res.json(result.rows)
-    }
-    catch(err){
-      res.send(err.message)
-    }
-  }
-  else{
-    res.send("not valid")
-  }
-})
-
-app.listen(PORT,()=>{
-    console.log(`running on port ${PORT}`)
-})
+  return (
+    <div className="sign-up login main-body">
+      {
+            prop.isLoading?<Backdrop
+            sx={(theme) => ({ color: '#fff', zIndex: theme.zIndex.drawer + 1 })}
+            open={true}
+          >
+            <CircularProgress color="inherit" />
+          </Backdrop>: <></>}
+          <div  className="header">
+            <img src={icon} style={{width:"50px", height:"auto"}} alt="add-user-male"/>  
+            <h2>Create Your Account</h2>
+            <p style={{color:"rgb(140,140, 140)"}}>Start Your Journey With Us!!</p>
+          </div>
+        <div className='form'>
+        <h2 style={{color:"rgb(71, 71, 71)", textAlign:"center"}}>REGESTER!</h2>
+        <form >
+            <div style={Mystyle}>
+                {nextPage===1?<TextField id="outlined-text-input" label="name"value={values.name} onChange={(e)=>handleChange(e)} placeholder="Name"  name="name"/>:<></>}
+                {nextPage ===1?<TextField id="outlined-text-input" label="mobile" type="text" value={values.mobile} onChange={(e)=>handleChange(e)} placeholder="mobile" name="mobile"/>:<></>}
+                {nextPage ===2?<TextField label="make a username" id="outlined-text-input"type="text" value={values.username} onChange={(e)=>handleChange(e)} placeholder="username" name="username"/>:<></>}
+                {nextPage===2?<TextField label="mail" id="outlined-text-input"type="text" value={values.mail} onChange={(e)=>handleChange(e)} placeholder="example: someone@gmailcom" name="mail"/>:<></>}
+                {nextPage ===4?<TextField label="chooose a password" id="outlined-password-input" type="password"  value={values.password} onChange={(e)=>handleChange(e)} placeholder="password" name="password"/>:<></>}
+                {nextPage===3?<TextField label="your Buisness" id="outlined-text-input" type="text" value={values.buisness} onChange={(e)=>handleChange(e)} placeholder="Buisness Name" name="buisness"/>:<></>}
+                {nextPage===3?<TextField label="No of rooms" id="outlined-text-input" type="text" value={values.rooms} onChange={(e)=>handleChange(e)} placeholder="rooms" name="rooms"/>:<></>}
+            </div>
+            <p className="error">{prop.error}</p>
+            <div className="buttons">
+            {nextPage>1?<button className="btn btn-primary" onClick={prev} style={MyStyle2}>prev</button>:<div></div>}
+            {nextPage<=3?<button className="btn btn-primary" onClick={next} style={MyStyle2}>next</button>:<></>}
+            {nextPage===4?<button type="submit" onClick={submit} className="btn btn-primary" style={MyStyle2}>Submit</button>:<></>}
+            </div>  
+        </form>
+        <p>Account already exist? <Link to="/login">Sign In</Link></p>
+        </div>
+    </div>
+  )
+}
+export default SignUp;
